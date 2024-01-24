403813ab726c641106ce2220a495060826d7f77a
Kongonii <heartsally0707@gmail.com>
2024-01-24 09:13:44 +0900

    kong-1

---
+++ moneyminder/Loading.js
@@ -0,0 +1,13 @@
+// Loading.js
+import React from 'react';
+import {Background, LoadingText} from './Styles';
+
+const Loading01 = () => {
+  return (
+    <Background>
+    <LoadingText>...</LoadingText>
+    </Background>
+  );
+};
+
+export default Loading01;
